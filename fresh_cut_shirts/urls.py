148f91415420d70c028aca9173bdc5e079975760
Charlie Van Norman <charlie@vannorman.ai>
2022-09-29 16:50:56 -0500

    added first shirt image

--- fresh_cut_shirts/urls.py
+++ fresh_cut_shirts/urls.py
@@ -9,15 +9,5 @@ urlpatterns = [
 
 
 	re_path(r'^$', fresh_cut_shirts.views.home),
-        re_path(r'^md/$', fresh_cut_shirts.views.render_md_blog),
-	re_path(r'^blog/$', fresh_cut_shirts.views.blog_base), 
 	re_path(r'^base/$', fresh_cut_shirts.views.simple_page('base.html')), 
-	re_path(r'^resume/$', fresh_cut_shirts.views.simple_page('resume.html')), 
-	re_path(r'^portfolio/$', fresh_cut_shirts.views.simple_page('portfolio.html')), 
-	re_path(r'^address/$', fresh_cut_shirts.views.simple_page('address.html')), 
-	re_path(r'^blog/(.*)$', fresh_cut_shirts.views.blog), 
-	re_path(r'^test/$', fresh_cut_shirts.views.test), 
-	re_path(r'^jammer/$', fresh_cut_shirts.views.jammer), 
-	re_path(r'^messages/whatyouwant$', fresh_cut_shirts.views.simple_page('messages/isthiswhatyouwant.html')), 
-
 ]
