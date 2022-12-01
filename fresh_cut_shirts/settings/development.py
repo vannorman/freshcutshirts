d3890e4f2289602e13b765ac5650dede67de9609
Charlie the Mad Proliferator <charlie@vannorman.ai>
2022-12-01 06:22:58 +0000

    fixed db owner issue that prevented apache using www-data to write to db (which was preventing logins)

--- fresh_cut_shirts/settings/development.py
+++ fresh_cut_shirts/settings/development.py
@@ -1,7 +1,4 @@
 from .base import *
-import os
-print('DEVELOPMENT SETTINGS LOADED from thread:' )
-print(os.getpid())
 
 DEBUG = True
 ALLOWED_HOSTS = [
--- fresh_cut_shirts/settings/production.py
+++ fresh_cut_shirts/settings/production.py
@@ -14,7 +14,7 @@ ALLOWED_HOSTS = [
 #    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
 #    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
 #    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'}
-]
+#]
 
 #DATABASES = {
 #    'default': {
